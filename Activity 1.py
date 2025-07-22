657fd2ee938cf37f2949d52211e1635397cbe8df
aviralgoel1 <aviralgoel1@gmail.com>
2025-07-22 12:07:21 +0100

    Lesson 1 Activity 1

---
+++ Activity 1.py
@@ -0,0 +1,35 @@
+print("hello world!")
+#printing my name and a fun fact
+print("hello, I am Aviral and I live in London")
+#integer 
+n=4
+print(n,"is an integer",type(n))
+#float
+x=5.5
+print(x,"is a float", type(x))
+print(x+n)
+#typecasting
+c=x+n
+d=int(c)
+print(d)
+print(d,type(d))
+#string
+s="5"
+print(s,"is a string", type(s))
+s1="5"
+print(s+s1)
+#boolean
+b=True
+print(b,"is a boolean", type(b))
+#input
+name=input("ever your name, please")
+print(name)
+#string operations
+print(name.upper())
+print(name.lower())
+print(name.join(" goel"))
+#string concatenation
+lname="goel"
+print(name+" "+lname)
+#reversing string
+print(name[::-1])
